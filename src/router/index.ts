5f85a4b051ab54ad626257ce230959920a834d88
Bosnier <notreplybosnier@outlook.com>
2025-08-25 20:21:00 +0400

    Create routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
+import MedsView from '@/views/MedsView.vue'
+import NotificationsView from '@/views/NotificationsView.vue'
+import SettingsView from '@/views/SettingsView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/meds', component: MedsView },
+    { path: '/notifications', component: NotificationsView },
+    { path: '/settings', component: SettingsView },
+  ],
 })
 
 export default router
---
+++ src/views/SettingsView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <span>settings</span>
+</template>
