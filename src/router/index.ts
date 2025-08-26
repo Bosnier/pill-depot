5a410479917d2fd52abe0a1b965f860e3dbb13ac
Bosnier <notreplybosnier@outlook.com>
2025-08-26 17:43:55 +0400

    Redirect from `/` to `/meds`

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/', redirect: '/meds' },
     { path: '/meds', component: MedsView },
     { path: '/notifications', component: NotificationsView },
     { path: '/settings', component: SettingsView },
