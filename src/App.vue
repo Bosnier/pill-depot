856e2c9d41f7ba52227c211a434e18801eefd5f7
Bosnier <notreplybosnier@outlook.com>
2025-08-26 17:42:59 +0400

    App and MedsView base layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Bolt, Cross, PillBottle } from 'lucide-vue-next'
+</script>
 
 <template>
-  <RouterLink to="/meds">{{ $t('tabs.meds') }}</RouterLink>
-  <RouterLink to="/notifications">{{ $t('tabs.notifications') }}</RouterLink>
-  <RouterLink to="/settings">{{ $t('tabs.settings') }}</RouterLink>
-  <RouterView />
+  <RouterView class="grow" />
+  <div class="nav">
+    <div class="nav-controls"></div>
+    <nav class="nav-tabs">
+      <RouterLink to="/meds"><Cross />{{ $t('tabs.meds') }}</RouterLink>
+      <RouterLink to="/notifications"><PillBottle />{{ $t('tabs.notifications') }}</RouterLink>
+      <RouterLink to="/settings"><Bolt />{{ $t('tabs.settings') }}</RouterLink>
+    </nav>
+  </div>
 </template>
+
+<style scoped>
+@reference '@/assets/main.css';
+
+.nav {
+  @apply sticky bottom-0 grid h-fit;
+  @apply md:bottom-2 md:mx-auto md:mt-2 md:w-lg md:max-w-dvw md:rounded-lg md:shadow-md;
+  @apply bg-stone-200 text-stone-500 shadow-stone-400/60 dark:bg-stone-800 dark:text-stone-400 dark:shadow-stone-950/60;
+}
+
+.nav-controls {
+  @apply has-[*]:border-b;
+  @apply border-stone-400 dark:border-stone-600;
+}
+
+.nav-tabs {
+  @apply grid h-16 grid-cols-3;
+
+  & > a {
+    @apply flex flex-col items-center justify-center gap-0.5 transition-colors;
+
+    &.router-link-active {
+      @apply text-black dark:text-stone-50;
+    }
+  }
+}
+</style>
--- src/views/MedsView.vue
+++ src/views/MedsView.vue
@@ -1,5 +1,56 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ArrowDownWideNarrow, Plus, Search } from 'lucide-vue-next'
+</script>
 
 <template>
-  <span>meds</span>
+  <div
+    v-bind="$attrs"
+    class="grid auto-rows-min content-end gap-1 p-1 md:grid-cols-2 lg:grid-cols-3 xl:container xl:mx-auto"
+  >
+    <div v-for="i in 400" :key="i" class="medicine">
+      <span class="medicine__id">{{ i }}</span>
+      <span class="medicine__name">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</span>
+      <span class="medicine__exp">until Aug 2028</span>
+    </div>
+  </div>
+  <Teleport to=".nav-controls">
+    <div class="flex h-10 px-4 md:px-1">
+      <button class="flex size-10 items-center justify-center"><Search /></button>
+      <button class="flex size-10 items-center justify-center"><Plus /></button>
+      <button class="ms-auto flex size-10 items-center justify-center"><ArrowDownWideNarrow /></button>
+      <select class="">
+        <option value="" selected>Created at</option>
+        <option value="">Expired at</option>
+        <option value="">Name</option>
+        <option value="">ID</option>
+      </select>
+    </div>
+  </Teleport>
 </template>
+
+<style scoped>
+@reference '@/assets/main.css';
+
+.medicine {
+  @apply grid rounded-md p-0.5;
+  @apply bg-stone-100 dark:bg-stone-900;
+  grid-template:
+    'id name' 1lh
+    'id exp' 1lh / 2lh 1fr;
+}
+
+.medicine__id {
+  @apply place-self-center font-mono text-lg;
+  grid-area: id;
+}
+
+.medicine__name {
+  @apply line-clamp-1;
+  grid-area: name;
+}
+
+.medicine__exp {
+  @apply text-sm text-stone-400;
+  grid-area: exp;
+}
+</style>
