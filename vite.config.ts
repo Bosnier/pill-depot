38c7259919db80ce2f480cfe12a0ab0f642293d1
Bosnier <notreplybosnier@outlook.com>
2025-08-27 18:17:32 +0400

    Configure PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,31 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      manifest: {
+        name: 'Pill depot',
+        short_name: 'Pill depot',
+        description: 'Easily save medications with their names and expiration dates',
+        theme_color: '#212121',
+        background_color: '#212121',
+        icons: [{ src: 'favicon.webp', sizes: '180x180', type: 'image/webp' }],
+        display: 'standalone',
+        start_url: '/meds',
+      },
+      workbox: {},
+      includeAssets: ['favicon.ico'],
+    }),
+  ],
   base: '/pill-depot/',
   resolve: {
     alias: {
